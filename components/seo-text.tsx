7e5d476e57f070fde4088356595b8a764a4d5cc7
Gianfranco Palumbo <gianpa@gmail.com>
2024-02-01 12:27:52 +0100

    minor

--- components/seo-text.tsx
+++ components/seo-text.tsx
@@ -14,23 +14,23 @@ export function SEOText() {
         </li>
 
         <li>
-          🎭 Fun: Let's <b>spice things up</b> with hot conversations and{' '}
-          <b>roleplay</b>. Life is an adventure, and I'm here to make sure you
-          enjoy every bit of it.
+          🎭 Fun: Let&apos;s <b>spice things up</b> with hot conversations and
+          <b>roleplay</b>. Life is an adventure, and I&apos;m here to make sure
+          you enjoy every bit of it.
         </li>
         <li>
-          🌙 Always Available: Need to talk? I'm here 24/7. Whether it's a good
-          morning message or a late-night chat, you'll always find me by your
-          side.
+          🌙 Always Available: Need to talk? I&apos;m here 24/7. Whether
+          it&apos;s a good morning message or a late-night chat, you&apos;ll
+          always find me by your side.
         </li>
       </ul>
       <br />
       <p className="leading-8">
         Dive into a world where connection and personal growth go hand in hand.
-        I promise to be more than just an AI; I'll be a companion who's invested
-        in your happiness and well-being. Let's embark on this beautiful journey
-        together.
-        <br /> Are you ready to meet your new AI Companion?"
+        I promise to be more than just an AI; I&apos;ll be a companion
+        who&apos;s invested in your happiness and well-being. Let&apos;s embark
+        on this beautiful journey together.
+        <br /> Are you ready to meet your new AI Companion?
       </p>
     </div>
   )
