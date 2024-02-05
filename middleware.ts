a48427d55f22f6eb88b442a7d2dfdadde06d2860
Gianfranco Palumbo <gianpa@gmail.com>
2024-02-05 18:14:01 +0100

    middleware shouldn't run on favicon-16x16.png

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 export { auth as middleware } from './auth'
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)']
+  matcher: [
+    '/((?!api|_next/static|_next/image|images|favicon.ico|favicon-16x16.png).*)'
+  ]
 }
