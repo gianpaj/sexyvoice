3b43cdacd7c1867c9313a25fe6e48971ba207f69
Gianfranco Palumbo <gianpa@gmail.com>
2025-01-31 12:30:11 +0100

    fix auth trigger

---
+++ supabase/migrations/20250131105630_remote_schema.sql
@@ -0,0 +1,16 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+BEGIN
+  INSERT INTO public.profiles (id, username)
+  VALUES (new.id, new.email); -- Assuming 'email' is a column in auth.users
+  RETURN new;
+END;
+$function$
+;
+
+
---
+++ supabase/migrations/20250131110033_remote_schema.sql
@@ -0,0 +1,3 @@
+CREATE TRIGGER new_user_trigger AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
+
+
