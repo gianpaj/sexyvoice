b58ddaf7fcd9f73bdb9c9478b789ec3935d07e00
Gianfranco Palumbo <gianpa@gmail.com>
2025-01-31 22:54:05 +0100

    add vercel analytics and speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/next';
 import { Inter } from 'next/font/google';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -19,6 +20,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
