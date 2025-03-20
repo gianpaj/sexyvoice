01a667790dde918d85396e7616e89d937e7dac92
Gianfranco Palumbo <gianpa@gmail.com>
2025-03-20 15:11:23 +0100

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,50 +1,50 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { i18n } from './lib/i18n/i18n-config';
-import { match as matchLocale } from '@formatjs/intl-localematcher';
-import { updateSession } from '@/lib/supabase/middleware';
-import Negotiator from 'negotiator';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { i18n } from './lib/i18n/i18n-config'
+import { match as matchLocale } from '@formatjs/intl-localematcher'
+import { updateSession } from '@/lib/supabase/middleware'
+import Negotiator from 'negotiator'
 
 function getLocale(request: NextRequest): string {
   const negotiatorHeaders = Object.fromEntries(
-    Array.from(request.headers.entries()).map(([key, value]) => [key, value]),
-  );
+    Array.from(request.headers.entries()).map(([key, value]) => [key, value])
+  )
 
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
 
-  return matchLocale(languages, i18n.locales, i18n.defaultLocale);
+  return matchLocale(languages, i18n.locales, i18n.defaultLocale)
 }
 
 export async function middleware(req: NextRequest) {
-  const pathname = req.nextUrl.pathname;
+  const pathname = req.nextUrl.pathname
 
   const pathnameIsMissingLocale = i18n.locales.every(
-    (locale) =>
+    locale =>
       !pathname.startsWith(`/${locale}/`) &&
       !pathname.startsWith('/auth') &&
       !pathname.startsWith('/api') &&
       !pathname.startsWith('/webhook') &&
-      pathname !== `/${locale}`,
-  );
+      pathname !== `/${locale}`
+  )
 
   // console.log({ pathnameIsMissingLocale });
 
   // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
     try {
-      const locale = getLocale(req);
-      return NextResponse.redirect(new URL(`/${locale}${pathname}`, req.url));
+      const locale = getLocale(req)
+      return NextResponse.redirect(new URL(`/${locale}${pathname}`, req.url))
     } catch (error) {
       if (pathname === '/') {
-        return NextResponse.redirect(new URL('/en', req.url));
+        return NextResponse.redirect(new URL('/en', req.url))
       }
-      return NextResponse.redirect(new URL(`/en${pathname}`, req.url));
+      return NextResponse.redirect(new URL(`/en${pathname}`, req.url))
     }
   }
 
-  return await updateSession(req);
+  return await updateSession(req)
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|audios).*)']
+}
