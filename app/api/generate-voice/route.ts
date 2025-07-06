4f70f2fa67260fcb319bb6b9322be49c0d2b8a1e
Gianfranco Palumbo <gianpa@gmail.com>
2025-07-06 19:28:33 +0200

    Fix voice style parameter to voice generation (Gemini)

--- app/api/generate-voice/route.ts
+++ app/api/generate-voice/route.ts
@@ -319,6 +319,20 @@ export async function POST(request: Request) {
     console.error(errorObj);
     console.error('Voice generation error:', error);
     if (error instanceof Error) {
+      // if Gemini error
+      if (error instanceof Error && error.message.includes('googleapis')) {
+        const message = JSON.parse(error.message);
+        // You exceeded your current quota
+        if (message.error.code === 429) {
+          return NextResponse.json(
+            {
+              error:
+                'We have exceeded our third-party API current quota, please try later or tomorrow',
+            },
+            { status: 500 },
+          );
+        }
+      }
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
     return NextResponse.json(
