1e83a61eb7ac2d6de53e1d08b7764335d0cb7f96
Gianfranco Palumbo <gianpa@gmail.com>
2025-08-17 13:52:43 +0200

    fix(generate-voice): limit error message. it depends on the AI model

--- scripts/db_backups.sh
+++ scripts/db_backups.sh
@@ -3,13 +3,13 @@
 # export SUPABASE_DB_URL=postgresql://postgres:xxx@db.yyyy.supabase.co:5432/postgres
 
 # Generate timestamp for backup files
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+TIMESTAMP=$(date +"%Y-%m-%dT%H_%M_%S")
 
-supabase db dump --db-url "$SUPABASE_DB_URL" -f "backups/roles_${TIMESTAMP}.sql" --role-only
+supabase db dump --db-url "$SUPABASE_DB_URL" -f "backups/${TIMESTAMP}_roles.sql" --role-only
 
-supabase db dump --db-url "$SUPABASE_DB_URL" -f "backups/schema_${TIMESTAMP}.sql"
+supabase db dump --db-url "$SUPABASE_DB_URL" -f "backups/${TIMESTAMP}_schema.sql"
 
-supabase db dump --db-url "$SUPABASE_DB_URL" -f "backups/data_${TIMESTAMP}.sql" --data-only --use-copy
+supabase db dump --db-url "$SUPABASE_DB_URL" -f "backups/${TIMESTAMP}_data.sql" --data-only --use-copy
 
 
 # https://github.com/sesto-dev/supabase-database-backup/blob/main/.github/workflows/backup.yaml
