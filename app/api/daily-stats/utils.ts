9178844e4b5131bd6c05b1cafaa0ca565b1ddf09
Gianfranco Palumbo <gianpa@gmail.com>
2025-10-11 21:10:58 +0200

    daily stats mask username
    - support 'date' URL search param in dev mode
    - remove 7 revenue stats
    - move functions to ./utils file

---
+++ app/api/daily-stats/utils.ts
@@ -0,0 +1,61 @@
+export function startOfDay(date: Date): Date {
+  return new Date(
+    Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()),
+  );
+}
+
+export function subtractDays(date: Date, days: number): Date {
+  return new Date(date.getTime() - days * 86400_000);
+}
+
+export function startOfMonth(date: Date): Date {
+  return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), 1));
+}
+
+export function startOfPreviousMonth(date: Date): Date {
+  return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth() - 1, 1));
+}
+
+export function formatChange(today: number, yesterday: number): string {
+  const diff = today - yesterday;
+  return diff >= 0 ? `+${diff}` : `${diff}`;
+}
+
+export function formatCurrencyChange(
+  current: number,
+  previous: number,
+): string {
+  const diff = current - previous;
+  return diff >= 0 ? `+${diff.toFixed(2)}` : `${diff.toFixed(2)}`;
+}
+
+export function reduceAmountUsd(acc: number, row: { metadata: any }) {
+  if (!row.metadata || typeof row.metadata !== 'object') {
+    console.log('Invalid metadata in row:', row);
+    return acc;
+  }
+  const { dollarAmount } = row.metadata as {
+    priceId: string;
+    dollarAmount: number;
+  };
+  if (typeof dollarAmount === 'number') {
+    return acc + dollarAmount;
+  }
+  return acc;
+}
+
+export function maskUsername(username?: string): string | undefined {
+  let maskedUsername = username;
+  if (username?.includes('@')) {
+    const [localPart, domain] = username.split('@');
+    if (localPart.length > 6) {
+      const first3 = localPart.slice(0, 3);
+      const last3 = localPart.slice(-3);
+      maskedUsername = `${first3}...${last3}@${domain}`;
+    } else if (localPart.length > 3) {
+      const first3 = localPart.slice(0, 3);
+      maskedUsername = `${first3}...@${domain}`;
+    }
+  }
+  return maskedUsername;
+}
