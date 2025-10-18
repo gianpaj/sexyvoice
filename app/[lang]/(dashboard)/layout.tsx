7e2739652716ea91fd135a87c5db461bd829f16d
Gianfranco P <899175+gianpaj@users.noreply.github.com>
2025-10-18 13:44:35 +0200

    Update layout.tsx

--- app/[lang]/(dashboard)/layout.tsx
+++ app/[lang]/(dashboard)/layout.tsx
@@ -9,13 +9,10 @@ export default async function DashboardLayout(props: {
   const { lang } = await props.params;
 
   const dict = await getDictionary(lang);
-  const halloweenDict = (await getDictionary(lang, 'promos')).halloweenBanner;
-
   return (
     <DashboardUI
       lang={lang}
       dict={dict.creditsSection}
-      halloweenDict={halloweenDict}
     >
       {props.children}
     </DashboardUI>
