31d6891fa723722e5f0c32be5388bda54e3b4778
Gianfranco Palumbo <gianpa@gmail.com>
2025-11-06 12:00:03 +0100

    address PR comments

--- components/services/toast.ts
+++ components/services/toast.ts
@@ -4,8 +4,6 @@ import { toast as sonnerToast } from 'sonner';
 
 import { TOAST_DURATION } from '@/lib/ui-constants';
 
-type PromiseT<T> = Promise<T>;
-
 interface PromiseData<ToastData> {
   loading?: React.ReactNode;
   success?: React.ReactNode | ((data: ToastData) => React.ReactNode);
@@ -21,37 +19,29 @@ export const toast = Object.assign(
     sonnerToast(message, { ...defaultToastOptions, ...data }),
   {
     ...sonnerToast,
-    success: (
-      message: React.ReactNode | React.ReactNode,
-      data?: ExternalToast,
-    ) => sonnerToast.success(message, { ...defaultToastOptions, ...data }),
+    success: (message: React.ReactNode, data?: ExternalToast) =>
+      sonnerToast.success(message, { ...defaultToastOptions, ...data }),
 
-    info: (message: React.ReactNode | React.ReactNode, data?: ExternalToast) =>
+    info: (message: React.ReactNode, data?: ExternalToast) =>
       sonnerToast.info(message, { ...defaultToastOptions, ...data }),
 
-    warning: (
-      message: React.ReactNode | React.ReactNode,
-      data?: ExternalToast,
-    ) => sonnerToast.warning(message, { ...defaultToastOptions, ...data }),
+    warning: (message: React.ReactNode, data?: ExternalToast) =>
+      sonnerToast.warning(message, { ...defaultToastOptions, ...data }),
 
-    error: (message: React.ReactNode | React.ReactNode, data?: ExternalToast) =>
+    error: (message: React.ReactNode, data?: ExternalToast) =>
       sonnerToast.error(message, { ...defaultToastOptions, ...data }),
 
     // custom() doesn't use the default duration
 
-    message: (
-      message: React.ReactNode | React.ReactNode,
-      data?: ExternalToast,
-    ) => sonnerToast.message(message, { ...defaultToastOptions, ...data }),
+    message: (message: React.ReactNode, data?: ExternalToast) =>
+      sonnerToast.message(message, { ...defaultToastOptions, ...data }),
 
     promise: <ToastData>(
-      promise: PromiseT<ToastData>,
+      promise: Promise<ToastData>,
       data?: PromiseData<ToastData>,
     ) => sonnerToast.promise(promise, { ...defaultToastOptions, ...data }),
 
-    loading: (
-      message: React.ReactNode | React.ReactNode,
-      data?: ExternalToast,
-    ) => sonnerToast.loading(message, { ...defaultToastOptions, ...data }),
+    loading: (message: React.ReactNode, data?: ExternalToast) =>
+      sonnerToast.loading(message, { ...defaultToastOptions, ...data }),
   },
 );
