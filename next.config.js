e7622daf6cbf0587a9bdeb27a3edcb01e516cc18
Gianfranco Palumbo <gianpa@gmail.com>
2026-01-01 21:01:06 +0100

    Use the appropriate configuration method for your framework to set up proxy rewrites. This ensures that ad-blockers, third party scripts, and more won't make BotID any less effective.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const { withContentlayer } = require('next-contentlayer2');
-// const { withBotId } = require('botid/next/config');
+const { withBotId } = require('botid/next/config');
 
 // TODO: generate CSP Header and add policy domains to on the the needed routes
 /**
@@ -98,7 +98,7 @@ let nextConfig = {
 
 nextConfig = withContentlayer(nextConfig);
 
-// nextConfig = withBotId(nextConfig);
+nextConfig = withBotId(nextConfig);
 
 // Injected content via Sentry wizard below
 if (process.env.NODE_ENV === 'production') {
