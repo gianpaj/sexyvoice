3dae2b4e3bfa6afba8bdfe23d9ae1fe76e456f50
gianpaj <gianpa@gmail.com>
2026-03-05 19:52:16 -0500

    fix: remove stable setIsGenerating from useCallback deps array (biome lint)

--- components/audio-generator.tsx
+++ components/audio-generator.tsx
@@ -337,7 +337,6 @@ export function AudioGenerator({
       splitSegments,
       isGenerating,
       selectedVoice,
-      setIsGenerating,
       showGenerationProgressToast,
       requestGenerateVoice,
       markSegmentGenerating,
