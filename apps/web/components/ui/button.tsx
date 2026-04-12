33ed95156882704b964336c08b765bfe7f6b26f9
gianpaj <gianpa@gmail.com>
2026-04-12 18:40:17 +0200

    fix / workaround: disabled button variants their bg color doesn't change on hover if disabled

--- apps/web/components/ui/button.tsx
+++ apps/web/components/ui/button.tsx
@@ -7,18 +7,18 @@ import { cn } from '@/lib/utils';
 
 // https://enhanced-button.vercel.app/
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium text-sm ring-offset-background transition-all duration-150 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium text-sm ring-offset-background transition-all duration-150 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2  disabled:bg-initial! disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow shadow hover:bg-primary-active',
+          'bg-primary text-primary-foreground shadow hover:bg-primary-active disabled:bg-primary',
         destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 disabled:bg-destructive',
         outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground disabled:bg-background',
         secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary-foreground/30',
+          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary-foreground/30 disabled:bg-secondary',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
